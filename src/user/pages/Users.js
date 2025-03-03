20da73f22f04ce01847eedf5fe6fec979c83aaf2
Alok Kumar <aloknh31@gmail.com>
2025-03-03 10:44:43 +0530

    added card and avatar components with the better ui

--- src/user/pages/Users.js
+++ src/user/pages/Users.js
@@ -1,17 +1,19 @@
 import React from 'react';
-import UserList from '../components/UserList';
-const Users = () => {
-    const USERS = [
-        {
-            id: 'u1',
-            name: 'Max',
-            placeCount: 2,
-            image: 'https://images.pexels.com/photos/220453/pexels-photo-220453.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1',
-        },
-    ]
-    return (
-        <UserList items={USERS} />
-    )
-}
 
-export default Users
+import UsersList from '../components/UsersList';
+
+const Users = () => {
+  const USERS = [
+    {
+      id: 'u1',
+      name: 'Max Schwarz',
+      image:
+        'https://images.pexels.com/photos/839011/pexels-photo-839011.jpeg?auto=compress&cs=tinysrgb&dpr=2&h=750&w=1260',
+      places: 3
+    }
+  ];
+
+  return <UsersList items={USERS} />;
+};
+
+export default Users;
