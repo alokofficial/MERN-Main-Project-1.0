aefee0964539c3370a4e9a002217bcef1eb32110
Alok Kumar <aloknh31@gmail.com>
2025-03-04 12:20:13 +0530

    Adding a Modal

--- src/places/components/PlaceItem.jsx
+++ src/places/components/PlaceItem.jsx
@@ -1,9 +1,22 @@
-import React from "react";
+import React, { useState } from "react";
 import './PlaceItem.css'
 import Card from "../../shared/components/UIElements/Card";
 import Button from "../../shared/components/FormElements/Button";
+import Modal from "../../shared/components/UIElements/Modal";
 const PlaceItem = (props) => {
-    return <li className="place-item">
+    const [showMap, setShowMap] = useState(false);
+    const openMapHandler = () => setShowMap(true);
+    const closeMapHandler = () => setShowMap(false);
+
+    return <>
+    <Modal show={showMap} onCancel={closeMapHandler} header={props.address} contentClass="place-item__modal-content" footerClass='place-item__modal-actions'
+    footer = {
+        <Button onClick={closeMapHandler}>CLOSE</Button>}
+    ><div className="map-container">
+        <h2>THE MAP!</h2>
+    </div>
+    </Modal>
+        <li className="place-item">
         <Card className="place-item__content">
         <div className="place-item__image">
             <img src={props.image} alt={props.title} />
@@ -14,12 +27,13 @@ const PlaceItem = (props) => {
             <p>{props.description}</p>
         </div>
         <div className="place-item__actions">
-            <Button inverse to={`/places/${props.id}`}>VIEW</Button>
+            <Button inverse onClick={openMapHandler}>VIEW ON MAP</Button>
             <Button to={`/places/${props.id}/edit`}>EDIT</Button>
             <Button danger onClick={props.onDelete}>DELETE</Button>
         </div>
         </Card>
     </li>
+    </>
 };
 
 export default PlaceItem;
