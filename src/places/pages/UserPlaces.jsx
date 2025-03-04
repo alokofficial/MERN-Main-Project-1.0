1b3dcb3385d3c647b46e26c5c5c8a9182c47f5e8
Alok Kumar <aloknh31@gmail.com>
2025-03-04 10:42:52 +0530

    Getting Route Params

--- src/places/pages/UserPlaces.jsx
+++ src/places/pages/UserPlaces.jsx
@@ -1,10 +1,11 @@
 import React from "react";
 import PlaceList from "../components/PlaceList";
+import { useParams } from "react-router-dom";
 
 const DUMMY_PLACES = [
     {
         id: 'p1',
-        title: 'Empire State Building',
+        title: 'Empire State Building1',
         description: 'One of the most famous sky scrapers in the world!',
         imageUrl: 'https://upload.wikimedia.org/wikipedia/commons/thumb/9/91/View_of_Empire_State_Building_from_Rockefeller_Center_New_York_City_dllu_Cropped.jpg/330px-View_of_Empire_State_Building_from_Rockefeller_Center_New_York_City_dllu_Cropped.jpg',
         address: '20 W 34th St, New York, NY 10001',
@@ -16,7 +17,7 @@ const DUMMY_PLACES = [
     },
     {
         id: 'p2',
-        title: 'Empire State Building',
+        title: 'Empire State Building2',
         description: 'One of the most famous sky scrapers in the world!',
         imageUrl: 'https://upload.wikimedia.org/wikipedia/commons/thumb/9/91/View_of_Empire_State_Building_from_Rockefeller_Center_New_York_City_dllu_Cropped.jpg/330px-View_of_Empire_State_Building_from_Rockefeller_Center_New_York_City_dllu_Cropped.jpg',
         address: '20 W 34th St, New York, NY 10001',
@@ -28,7 +29,7 @@ const DUMMY_PLACES = [
     },
     {
         id: 'p3',
-        title: 'Empire State Building',
+        title: 'Empire State Building3',
         description: 'One of the most famous sky scrapers in the world!',
         imageUrl: 'https://upload.wikimedia.org/wikipedia/commons/thumb/9/91/View_of_Empire_State_Building_from_Rockefeller_Center_New_York_City_dllu_Cropped.jpg/330px-View_of_Empire_State_Building_from_Rockefeller_Center_New_York_City_dllu_Cropped.jpg',
         address: '20 W 34th St, New York, NY 10001',
@@ -40,7 +41,9 @@ const DUMMY_PLACES = [
     }
 ]
 const UserPlaces = () => {
-    return <PlaceList items={DUMMY_PLACES}/>
+    const userId = useParams().userId;
+    const loadedPlaces = DUMMY_PLACES.filter(place => place.creator === userId);
+    return <PlaceList items={loadedPlaces}/>
 };
 
 export default UserPlaces;
