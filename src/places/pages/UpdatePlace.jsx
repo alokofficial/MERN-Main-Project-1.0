17d8f304f014be880cbfba7a9de1be75e63a214a
Alok Kumar <aloknh31@gmail.com>
2025-03-05 12:04:18 +0530

    Starting Work on the "Update Place" Page

---
+++ src/places/pages/UpdatePlace.jsx
@@ -0,0 +1,90 @@
+import React from "react";
+import { useParams } from "react-router-dom";
+import Input from "../../shared/components/FormElements/Input";
+import {
+  VALIDATOR_MINLENGTH,
+  VALIDATOR_REQUIRE,
+} from "../../shared/util/validators";
+import Button from "../../shared/components/FormElements/Button";
+
+const DUMMY_PLACES = [
+  {
+    id: "p1",
+    title: "Empire State Building1",
+    description: "One of the most famous sky scrapers in the world!",
+    imageUrl:
+      "https://upload.wikimedia.org/wikipedia/commons/thumb/9/91/View_of_Empire_State_Building_from_Rockefeller_Center_New_York_City_dllu_Cropped.jpg/330px-View_of_Empire_State_Building_from_Rockefeller_Center_New_York_City_dllu_Cropped.jpg",
+    address: "20 W 34th St, New York, NY 10001",
+    location: {
+      lat: 12.9101576,
+      lng: 77.6019046,
+    },
+    creator: "u1",
+  },
+  {
+    id: "p2",
+    title: "Empire State Building2",
+    description: "One of the most famous sky scrapers in the world!",
+    imageUrl:
+      "https://upload.wikimedia.org/wikipedia/commons/thumb/9/91/View_of_Empire_State_Building_from_Rockefeller_Center_New_York_City_dllu_Cropped.jpg/330px-View_of_Empire_State_Building_from_Rockefeller_Center_New_York_City_dllu_Cropped.jpg",
+    address: "20 W 34th St, New York, NY 10001",
+    location: {
+      lat: 12.9101576,
+      lng: 77.6019046,
+    },
+    creator: "u2",
+  },
+  {
+    id: "p3",
+    title: "Empire State Building3",
+    description: "One of the most famous sky scrapers in the world!",
+    imageUrl:
+      "https://upload.wikimedia.org/wikipedia/commons/thumb/9/91/View_of_Empire_State_Building_from_Rockefeller_Center_New_York_City_dllu_Cropped.jpg/330px-View_of_Empire_State_Building_from_Rockefeller_Center_New_York_City_dllu_Cropped.jpg",
+    address: "20 W 34th St, New York, NY 10001",
+    location: {
+      lat: 12.9101576,
+      lng: 77.6019046,
+    },
+    creator: "u3",
+  },
+];
+const UpdatePlace = () => {
+  const { placeId } = useParams();
+  const identifiedPlace = DUMMY_PLACES.find((p) => p.id === placeId);
+
+  if (!identifiedPlace) {
+    return (
+      <div className="center">
+        <h2>Could not find place</h2>
+      </div>
+    );
+  }
+  return (
+    <form>
+      <Input
+        id="title"
+        element="input"
+        type="text"
+        label="Title"
+        validators={[VALIDATOR_REQUIRE()]}
+        errorText="Please enter a valid title"
+        value={identifiedPlace.title}
+        valid={true}
+        onInput={() => {}}
+      />
+      <Input
+        id="description"
+        element="textarea"
+        label="Description"
+        validators={[VALIDATOR_MINLENGTH(5)]}
+        errorText="Please enter a valid description"
+        value={identifiedPlace.description}
+        valid={true}
+        onInput={() => {}}
+      />
+      <Button type="submit" disabled={true}>UPDATE PLACE</Button>
+    </form>
+  );
+};
+
+export default UpdatePlace;
