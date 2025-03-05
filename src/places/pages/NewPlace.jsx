86805f8348238f6bc0de78c2921cc60a6b030af0
Alok Kumar <aloknh31@gmail.com>
2025-03-05 09:36:53 +0530

    Sharing Input Values & Adding Multiple Inputs

--- src/places/pages/NewPlace.jsx
+++ src/places/pages/NewPlace.jsx
@@ -1,8 +1,14 @@
-import React from "react";
+import React, { useCallback } from "react";
 import Input from "../../shared/components/FormElements/Input";
-import {VALIDATOR_REQUIRE} from "../../shared/util/validators";
+import {
+  VALIDATOR_MINLENGTH,
+  VALIDATOR_REQUIRE,
+} from "../../shared/util/validators";
 import "./NewPlace.css";
 const NewPlace = () => {
+  const titleInputHandler = useCallback((id, value, isValid) => {}, []);
+
+  const descriptionInputHandler = useCallback((id, value, isValid) => {}, []);
   return (
     <form className="place-form">
       <Input
@@ -12,6 +18,16 @@ const NewPlace = () => {
         type="text"
         validator={[VALIDATOR_REQUIRE()]}
         errorText="Please enter a valid title."
+        onInput={titleInputHandler}
+      />
+      <textarea
+        id="description"
+        label="Description"
+        element="textarea"
+        type="text"
+        validator={[VALIDATOR_MINLENGTH(5)]}
+        errorText="Please enter a valid description(at least 5 characters)."
+        onInput={descriptionInputHandler}
       />
     </form>
   );
--- src/shared/components/FormElements/Input.jsx
+++ src/shared/components/FormElements/Input.jsx
@@ -1,4 +1,4 @@
-import React, { useReducer } from "react";
+import React, { useReducer, useEffect } from "react";
 import { validate } from "../../util/validators";
 import "./Input.css";
 const inputReducer = (state, action) => {
@@ -24,6 +24,14 @@ const Input = (props) => {
     isValid: false,
     isTouched: false,
   });
+
+  const { id, onInput } = props;
+  const { value, isValid } = inputState;
+
+  useEffect(() => {
+    onInput(id, value, isValid);
+  }, [id, value, isValid, onInput]);
+
   const changeHandler = (event) => {
     dispatch({
       type: "CHANGE",
@@ -35,7 +43,7 @@ const Input = (props) => {
     dispatch({
       type: "TOUCH",
     });
-  }
+  };
   const element =
     props.element === "input" ? (
       <input
