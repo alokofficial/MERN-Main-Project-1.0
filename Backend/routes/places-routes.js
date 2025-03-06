0f524cb93c1496a26013134fc37ee2d4f688e4eb
Alok Kumar <aloknh31@gmail.com>
2025-03-06 13:09:53 +0530

    Adding our own Error Model

--- Backend/routes/places-routes.js
+++ Backend/routes/places-routes.js
@@ -1,6 +1,6 @@
 import express from "express";
 import DUMMY_PLACES from "../db/dummy-data.js";
-import e from "express";
+import HttpError from "../models/http-error.js";
 
 const router = express.Router();
 
@@ -9,9 +9,7 @@ router.get("/:pid", (req, res, next) => {
   const placeId = req.params.pid; //{pid: 'p1'}
   const place = DUMMY_PLACES.find((p) => p.id === placeId);
   if(!place){
-    const error = new Error("Place not found place for the provided id");
-    error.code = 404;
-    throw error
+    throw new HttpError("Place not found", 404)
   }
   console.log("Get Request in Places");
   res.json({ place });
@@ -21,9 +19,7 @@ router.get('/user/:uid', (req, res, next) => {
       const userId = req.params.uid;
       const places = DUMMY_PLACES.filter(p => p.creator === userId);
       if(!places){
-        const error = new Error("Place not found place for the provided user id");
-        error.code = 404;
-        return next(error)
+       return next(new HttpError("Place not found for this user", 404))
       }
       res.json({places});
      
