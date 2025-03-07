007e30564876be02265caed827ccc5fd35e817c5
Alok Kumar <aloknh31@gmail.com>
2025-03-07 11:19:19 +0530

    Handling Errors for Unsupported Routes

--- Backend/app.js
+++ Backend/app.js
@@ -2,6 +2,7 @@ import express from "express";
 import bodyParser from "body-parser";
 
 import placesRoutes from "./routes/places-routes.js";
+import HttpError from "./models/http-error.js";
 
 const app = express();
 
@@ -9,6 +10,11 @@ app.use(bodyParser.json())
 // app.use(bodyParser.urlencoded({extended:true}))
 
 app.use('/api/places',placesRoutes);
+
+app.use((req, res, next) => {
+    const error = new HttpError('Could not find this route', 404)
+    throw error
+})
 
 app.use((error,req, res, next) => { //error middleware by express
     if(req.headerSent){
