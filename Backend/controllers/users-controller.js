7cc8935a1565ecd309da93460ef13490ab95ccf6
Alok Kumar <aloknh31@gmail.com>
2025-03-11 14:36:15 +0530

    Using the User Model for Signup

--- Backend/controllers/users-controller.js
+++ Backend/controllers/users-controller.js
@@ -1,33 +1,52 @@
-import { v4 as uuid } from "uuid";
+import User from "../models/user.js";
+
 import DUMMY_USERS from "../db/users-dummy-data.js";
 import HttpError from "../models/http-error.js";
 import { validationResult } from "express-validator";
 export const getUsers = (req, res, next) => {
    res.json({users:DUMMY_USERS})
 };
-export const userSignup = (req, res, next) => {
+export const userSignup = async(req, res, next) => {
     const errors = validationResult(req);
     if (!errors.isEmpty()) {
-      throw new HttpError("Invalid inputs passed, please check your data", 422);
+      return next(new HttpError("Invalid inputs passed, please check your data", 422));
     }
     
-    const {name, email, password} = req.body
+    const {name, email, password,places} = req.body
 
-    const hasUser = DUMMY_USERS.find(u=>u.email===email)
-    if(hasUser){
-        throw new HttpError('User already exists', 422)
+    let existingUser;
+    try {
+        existingUser =  await User.findOne({email:email})
+    } catch (error) {
+        const err = new HttpError(
+            "Signing up failed, please try again.",
+            500
+        )
     }
-
-    const createdUser={
-        id:uuid(),
+    if(existingUser){
+        const error = new HttpError("User exists already with this email, please login instead", 422);
+        return next(error)
+    }
+    const createdUser= new User({
         name,
         email,
-        password
+        password,
+        image:'https://upload.wikimedia.org/wikipedia/commons/thumb/f/fb/Newtown%2C_Wales.jpg/360px-Newtown%2C_Wales.jpg',
+        places,
+    })
+    try {
+        await createdUser.save()
+    } catch (error) {
+        const err = new HttpError(
+            "Signing up failed-2, please try again.",
+            500
+        )
+        return next(err)
     }
 
     DUMMY_USERS.push(createdUser)
 
-    res.status(201).json({user:createdUser})
+    res.status(201).json({user:createdUser.toObject({getters:true})})
 
 };
 export const userLogin = (req, res, next) => {
--- Backend/models/user.js
+++ Backend/models/user.js
@@ -1,20 +1,15 @@
 import mongoose from "mongoose";
-import uniqueValidator from "mongoose-unique-validator";
+
+
 
 const userSchema = new mongoose.Schema({
     name: { type: String, required: true },
     email: { type: String, required: true, unique: true },
     password: { type: String, required: true, minLength: 6 },
-    image: { type: String, required: true },
-    places: [
-        { 
-            type: mongoose.Schema.Types.ObjectId, 
-            ref: 'Place' }
-    ],
+    image: { type: String },
+    places: { type: String},
     resetToken: { type: String },
     resetTokenExpiration: { type: Date }
 }, { timestamps: true });
-
-userSchema.plugin(uniqueValidator);
 
 export default mongoose.model('User', userSchema);
