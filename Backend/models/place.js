5be567ed454e6dd73ef38867c19e012775bf4c25
Alok Kumar <aloknh31@gmail.com>
2025-03-11 10:52:04 +0530

    Creating the Place Schema & Model

---
+++ Backend/models/place.js
@@ -0,0 +1,18 @@
+import mongoose from "mongoose";
+
+const placeSchema = new mongoose.Schema(
+  {
+    title: { type: String, required: true },
+    description: { type: String, required: true },
+    image: { type: String, required: true },
+    address: { type: String, required: true },
+    location: {
+      lat: { type: Number, required: true },
+      lng: { type: Number, required: true },
+    },
+    creator: { type: String, required: true },
+  },
+  { timestamps: true }
+);
+
+export default mongoose.model("Place", placeSchema);
