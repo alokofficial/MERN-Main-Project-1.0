ad4554bd99290a1abe947e43a6c7ab2136896e16
Alok Kumar <aloknh31@gmail.com>
2025-03-14 10:23:42 +0530

    Handling CORS Errors

--- Backend/app.js
+++ Backend/app.js
@@ -10,7 +10,12 @@ const app = express();
 
 app.use(bodyParser.json())
 // app.use(bodyParser.urlencoded({extended:true}))
-
+app.use((req, res, next) => {
+    res.setHeader('Access-Control-Allow-Origin', '*');
+    res.setHeader('Access-Control-Allow-Headers', 'Origin, X-Requested-With, Content-Type, Accept, Authorization');
+    res.setHeader('Access-Control-Allow-Methods', 'GET, POST, PATCH, DELETE');
+    next(); 
+})
 app.use('/api/places',placesRoutes);
 app.use('/api/users',usersRoutes);
 
@@ -26,7 +31,7 @@ app.use((error,req, res, next) => { //error middleware by express
     res.status(error.code || 500)
     res.json({message: error.message || 'An unknown error occurred'})
 })
-mongoose.connect(`mongodb://localhost:27017/places`).then(
+mongoose.connect(`mongodb://localhost:27017/mern_project`).then(
     () => {
         app.listen(5000, () => {
             console.log("Server is running on port 5000")
--- Frontend/src/user/pages/Auth.jsx
+++ Frontend/src/user/pages/Auth.jsx
@@ -1,87 +1,139 @@
-import React,{useState,useContext} from "react";
+import React, { useState, useContext } from "react";
 import Card from "../../shared/components/UIElements/Card";
 import Button from "../../shared/components/FormElements/Button";
 import Input from "../../shared/components/FormElements/Input";
-import './Auth.css';
-import { VALIDATOR_EMAIL, VALIDATOR_MINLENGTH, VALIDATOR_REQUIRE } from "../../shared/util/validators";
+import "./Auth.css";
+import {
+  VALIDATOR_EMAIL,
+  VALIDATOR_MINLENGTH,
+  VALIDATOR_REQUIRE,
+} from "../../shared/util/validators";
 import { useForm } from "../../shared/hooks/form-hook";
 import { AuthContext } from "../../shared/context/auth-context";
 
 const Auth = () => {
-    const auth = useContext(AuthContext);
-    const [isLoginMode, setIsLoginMode] = useState(true);
-    const [formState, inputHandler, setFormData] = useForm({
-        email: {
-            value: '',
-            isValid: false
-        },
-        password: {
-            value: '',
-            isValid: false
-        }
-    },false)
+  const auth = useContext(AuthContext);
+  const [isLoginMode, setIsLoginMode] = useState(true);
+  const [formState, inputHandler, setFormData] = useForm(
+    {
+      email: {
+        value: "",
+        isValid: false,
+      },
+      password: {
+        value: "",
+        isValid: false,
+      },
+    },
+    false
+  );
 
-    const switchModeHandler = () => {
-        if (isLoginMode) {
-            setFormData( {
-                ...formState.inputs,
-                name: undefined
-            },
-            formState.inputs.email.isValid && formState.inputs.password.isValid)
-        }else{
-            setFormData({
-                ...formState.inputs,
-                name: {
-                    value: '',
-                    isValid: false
-                }
-            },
-            false
-            )
-        }
-        setIsLoginMode(prevMode => !prevMode);
+  const switchModeHandler = () => {
+    if (isLoginMode) {
+      setFormData(
+        {
+          ...formState.inputs,
+          name: undefined,
+        },
+        formState.inputs.email.isValid && formState.inputs.password.isValid
+      );
+    } else {
+      setFormData(
+        {
+          ...formState.inputs,
+          name: {
+            value: "",
+            isValid: false,
+          },
+        },
+        false
+      );
     }
-    const authSubmitHandler = (event) => {
-        event.preventDefault();
-        console.log(formState.inputs);
-        auth.login();
+    setIsLoginMode((prevMode) => !prevMode);
+  };
+  const authSubmitHandler = async (event) => {
+    event.preventDefault();
+    if (isLoginMode) {
+      try {
+        const response = await fetch("http://localhost:5000/api/users/login", {
+          method: "POST",
+          headers: {
+            "Content-Type": "application/json",
+          },
+          body: JSON.stringify({
+            email: formState.inputs.email.value,
+            password: formState.inputs.password.value,
+          }),
+        });
+        const responseData = await response.json();
+        console.log(responseData);
+      } catch (error) {
+        console.log(error);
+      }
+    } else {
+      try {
+        const response = await fetch("http://localhost:5000/api/users/signup", {
+          method: "POST",
+          headers: {
+            "Content-Type": "application/json",
+          },
+          body: JSON.stringify({
+            name: formState.inputs.name.value,
+            email: formState.inputs.email.value,
+            password: formState.inputs.password.value,
+          }),
+        });
+        const responseData = await response.json();
+        console.log(responseData);
+      } catch (error) {
+        console.log(error);
+      }
     }
-    return (
-        <Card className="authentication">
-            <h2>Login Required!</h2><hr/>
-            <form onSubmit={authSubmitHandler}>
-                {!isLoginMode && <Input
-                    element="input"
-                    id="name"
-                    type="text"
-                    label="Username"
-                    validators={[VALIDATOR_REQUIRE()]}
-                    errorText="Please enter a valid username, at least 5 characters."
-                    onInput={inputHandler}
-                />}
-                <Input
-                    element="input"
-                    id="email"
-                    type="email"
-                    label="E-Mail"
-                    validators={[VALIDATOR_EMAIL()]}
-                    errorText="Please enter a valid email address."
-                    onInput={inputHandler}
-                />
-                <Input
-                    element="input"
-                    id="password"
-                    type="password"
-                    label="Password"
-                    validators={[VALIDATOR_MINLENGTH(6)]}
-                    errorText="Please enter a valid password, at least 6 characters."
-                    onInput={inputHandler}
-                />
-                <Button type="submit" disabled={!formState.isValid }>{isLoginMode ? 'Login' : 'Signup'}</Button>
-            </form>
-            <Button onClick={switchModeHandler} inverse >Switch to {isLoginMode ? 'Signup' : 'Login'} </Button>
-        </Card>
-    );
+    auth.login();
+  };
+  return (
+    <Card className="authentication">
+      <h2>Login Required!</h2>
+      <hr />
+      <form onSubmit={authSubmitHandler}>
+        {!isLoginMode && (
+          <Input
+            element="input"
+            id="name"
+            type="text"
+            label="Username"
+            validators={[VALIDATOR_REQUIRE()]}
+            errorText="Please enter a valid username, at least 5 characters."
+            onInput={inputHandler}
+          />
+        )}
+        <Input
+          element="input"
+          id="email"
+          type="email"
+          label="E-Mail"
+          validators={[VALIDATOR_EMAIL()]}
+          errorText="Please enter a valid email address."
+          onInput={inputHandler}
+        />
+        <Input
+          element="input"
+          id="password"
+          type="password"
+          label="Password"
+          validators={[VALIDATOR_MINLENGTH(6)]}
+          errorText="Please enter a valid password, at least 6 characters."
+          onInput={inputHandler}
+        />
+        <Button type="submit" disabled={!formState.isValid}>
+          {isLoginMode ? "Login" : "Signup"}
+        </Button>
+      </form>
+      <Button onClick={switchModeHandler} inverse>
+        Switch to {isLoginMode ? "Signup" : "Login"}{" "}
+      </Button>
+    </Card>
+  );
 };
 
 export default Auth;
