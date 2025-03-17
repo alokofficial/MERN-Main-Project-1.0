b3f009a1792635ec12acbf8a0c7ddf8059cce60b
Alok Kumar <aloknh31@gmail.com>
2025-03-17 11:28:45 +0530

    Authorization on "Delete Places"

--- Backend/controllers/places-controller.js
+++ Backend/controllers/places-controller.js
@@ -165,6 +165,11 @@ export const deletePlace = async(req, res, next) => {
      return next(error);
    }
 
+   if(place.creator.id !== req.userData.userId){
+     const error = new HttpError("You are not allowed to delete this place", 401);
+     return next(error);
+   }
+
    const imagePath = place.image;
    try {
      const sess = await mongoose.startSession();
