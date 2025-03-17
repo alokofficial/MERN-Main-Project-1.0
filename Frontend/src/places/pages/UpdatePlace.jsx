7e1c0de247d16ae78909aa2b77a0fb30af52b741
Alok Kumar <aloknh31@gmail.com>
2025-03-17 10:22:16 +0530

    Using Tokens to Update and Delete Places

--- Frontend/src/places/pages/UpdatePlace.jsx
+++ Frontend/src/places/pages/UpdatePlace.jsx
@@ -127,6 +127,7 @@ const UpdatePlace = () => {
         }),
         {
           "Content-Type": "application/json",
+          Authorization: "Bearer " + auth.token,
         }
       )
       history.push(`/${auth.userId}/places`)
